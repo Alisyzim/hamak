589096baf688303e7e78ea8cfe8704f12ecd2cff
Alisyzim <147917882+Alisyzim@users.noreply.github.com>
2024-06-18 04:53:42 +0300

    Add files via upload

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
      domain="dev-bnag72hdxz0u12qv.eu.auth0.com"
      clientId="GflqkyBHaHnGFXKfSd8xaZnz3SSXYkNa"
      authorizationParams={{
-      redirect_uri: "http://localhost:5173"
+      redirect_uri: "https://hamak-ali-zamzams-projects.vercel.app"
      }}
      audience="http://localhost:8000"
      scope="openid profile email"
