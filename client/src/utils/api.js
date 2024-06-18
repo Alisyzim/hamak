6e0a57c2d8495fd6b7e9a59f46457a1eeb352eb8
Alisyzim <147917882+Alisyzim@users.noreply.github.com>
2024-06-18 04:44:45 +0300

    Add files via upload

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://hamak-puce.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
