3b6e8f5dadc8d35cd253b35e65ec9ec837bd4079
ali <ali0936781321@gmail.com>
2024-07-07 21:06:44 +0300

    22

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
      authorizationParams={{
       redirect_uri: "https://hamak-two.vercel.app"
      }}
-     audience="http://localhost:8000"
+     audience="https://hamak-server.vercel.app"
      scope="openid profile email"
     >
       <App />
