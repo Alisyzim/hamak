83e2c912bc65656a44566cbbacab6ca47b95fa09
ali <ali0936781321@gmail.com>
2024-07-24 14:41:35 +0300

    outh

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -1,7 +1,7 @@
 import {auth} from 'express-oauth2-jwt-bearer'
 
 const jwtCheck = auth({
-    audience: "http://localhost:8000/",
+    audience: "http://localhost:8000",
     issuerBaseURL: "dev-bnag72hdxz0u12qv.eu.auth0.com",
     tokenSigningAlg: "RS256"
 })
